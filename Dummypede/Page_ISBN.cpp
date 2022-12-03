de62fded6fe3a327fed093e62c16194a8383a80b
aikawa <aikawa3311@gmail.com>
2022-12-04 06:49:13 +0900

    説明の加筆とUIの位置調整

--- Dummypede/Page_ISBN.cpp
+++ Dummypede/Page_ISBN.cpp
@@ -27,7 +27,8 @@ void Page_ISBN::Draw() const{
 	if(page_name == U"ISBN Code") {
 		// ISBN Code
 		FontAsset(U"RegularThinMintyo")(U"書籍を特定するためのコード。\n現在では数字13桁で表す方式が主流である。").draw(base + Vec2(10, 120), Palette::Black);
-		FontAsset(U"HugeMintyo")(U"9784XXXXXXXXX").drawAt((size/2).movedBy( 0, (int)base.y - 50), Palette::Black);
+		// FontAsset(U"HugeMintyo")(U"9784XXXXXXXXX").drawAt((size/2).movedBy( 0, (int)base.y - 50), Palette::Black);
+		FontAsset(U"HugeMintyo")(U"9784XXXXXXXXX").drawAt(size.x/2, base.y + 200, Palette::Black);
 		Line(base + Vec2(90 - base.x, 226), base + Vec2(182 - base.x, 226)).draw(4, Palette::Black);
 		Line(base + Vec2(190 - base.x, 226), base + Vec2(410 - base.x, 226)).draw(4, Palette::Black);
 		FontAsset(U"Regular")(U"4桁(固定)").draw(base + Vec2(94 - base.x, 234), Palette::Black);
@@ -35,6 +36,7 @@ void Page_ISBN::Draw() const{
 		FontAsset(U"RegularThinMintyo")(U"日本では上位4桁が9784で固定されるため、\n実質下位9桁によって書籍を特定することが\n可能である。").draw(base + Vec2(10, 280), Palette::Black);
 		FontAsset(U"RegularThinMintyo")(U"外部リンク").draw(base + Vec2(10, 360), Palette::Black);
 		FontAsset(U"RegularThinMintyo")(U"https://www.books.or.jp/").draw(base + Vec2(10, 380), Palette::Blue);
+		FontAsset(U"RegularThinMintyo")(U"13桁のISBN Codeにより書籍の検索が可能。").draw(base + Vec2(10, 407), Palette::Black);
 		Line(base + Vec2(10, 402), base + Vec2(250, 402)).draw(1, Palette::Blue);
 	}
 	else if (page_name == U"Mikipedia") {
@@ -74,10 +76,10 @@ void Page_ISBN::Draw() const{
 	else {
 		// Not Found & 寄付ページ
 		FontAsset(U"RegularThinMintyo")(U"問い合わせに合致する検索結果は\nありませんでした。").draw(base + Vec2(10, 120), Palette::Black);
-		RectF(base.movedBy(0, 205), Vec2(460, 195)).drawFrame(4, Palette::Red);
-		RectF(base.movedBy(0, 205), Vec2(460, 30)).draw(Palette::Red);
-		FontAsset(U"BoldRegular")(U"寄付のお願い（ご一読ください）").draw(base + Vec2(10, 205), Palette::White);
-		FontAsset(U"RegularThinMintyo")(U"Mikipediaは多くの支援により成立していますが、\n依然として厳しい資金難に直面しています。\nMikipediaを、あなたの力で支えませんか。").draw(base + Vec2(10, 240), Palette::Black);
+		RectF(base.movedBy(0, 225), Vec2(460, 195)).drawFrame(4, Palette::Red);
+		RectF(base.movedBy(0, 225), Vec2(460, 30)).draw(Palette::Red);
+		FontAsset(U"BoldRegular")(U"寄付のお願い（ご一読ください）").draw(base + Vec2(10, 225), Palette::White);
+		FontAsset(U"RegularThinMintyo")(U"Mikipediaは多くの支援により成立していますが、\n依然として厳しい資金難に直面しています。\nMikipediaを、あなたの力で支えませんか。").draw(base + Vec2(10, 260), Palette::Black);
 		if (active && donate_button.movedBy(pos).intersects(Cursor::PosF())) {
 			donate_button.drawFrame(3, Palette::Blueviolet).draw(Palette::Red);
 			FontAsset(U"Regular")(U"莉翫縺仙ｯ?ｻ倥繧").drawAt(donate_button.center(), Palette::White);
@@ -86,7 +88,7 @@ void Page_ISBN::Draw() const{
 			donate_button.drawFrame(3, Palette::Darkgray).draw(Palette::Ghostwhite);
 			FontAsset(U"Regular")(U"今すぐ寄付する").drawAt(donate_button.center(), Palette::Black);
 		}
-		FontAsset(U"RegularThinMintyo")(U"寄付のご協力ありがとうございます。").draw(base + Vec2(10, 360), Palette::Black);
+		FontAsset(U"RegularThinMintyo")(U"寄付のご協力ありがとうございます。").draw(base + Vec2(10, 380), Palette::Black);
 	}
 
 	/*Vec2 base(30, 60);
@@ -176,13 +178,13 @@ void Page_ISBN::jump_page(){
 }
 
 Page_ISBN::Page_ISBN(WindowSystemManager& manager, String const & str)
-	:WindowSystem(Vec2(350, 150), Size(500, 500), str + U" - Mikipedia"),
+	:WindowSystem(Vec2(350, 150), Size(500, 520), str + U" - Mikipedia"),
 	manager(manager),
 	page_name(str),
 	text(U""),
 	textarea_rect(Vec2(240, 60), Size(220, 30)),
 	textarea_active(true),
-	donate_button(Vec2(120, 394), Vec2(250, 35)),
+	donate_button(Vec2(120, 414), Vec2(250, 35)),
 	link_button(Vec2(40, 465), Vec2(240, 25)){
 
 }
